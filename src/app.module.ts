a63210a4220a05fbae9b97bcc7ccdc80a3a14255
VarScript <vargassanchezfabio01@gmail.com>
2023-10-17 17:59:40 -0500

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,11 @@
+import { join } from 'path';
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+
+@Module({
+  imports: [],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
