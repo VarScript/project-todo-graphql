e8f7eb761c325ff8386800becfa863ff235649a6
VarScript <vargassanchezfabio01@gmail.com>
2023-10-18 15:16:25 -0500

    update todo

--- src/todo/todo.resolver.ts
+++ src/todo/todo.resolver.ts
@@ -1,8 +1,13 @@
-import { Args, Int, Mutation, Query, Resolver } from '@nestjs/graphql';
+import {
+  Args,
+  Int,
+  Mutation,
+  Query,
+  Resolver,
+} from '@nestjs/graphql';
 import { Todo } from './entity/todo.entity';
 import { TodoService } from './todo.service';
-import { Param, ParseIntPipe } from '@nestjs/common';
-import { CreateTodoInput } from './dto/inputs/create-todo.input';
+import { CreateTodoInput, UpdateTodoInput } from './dto/inputs';
 
 @Resolver()
 export class TodoResolver {
@@ -14,16 +19,27 @@ export class TodoResolver {
   }
 
   @Query(() => Todo, { name: 'todo' })
-  findOne(@Args('id', { type: () => Int }) id: number): Todo {
+  findOne(
+    @Args('id', { type: () => Int }) id: number,
+  ): Todo {
     return this.todoService.findOne(id);
   }
 
   @Mutation(() => Todo, { name: 'createTodo' })
-  createTodo(@Args('createTodoInput') createTodoInput: CreateTodoInput) {
+  createTodo(
+    @Args('createTodoInput')
+    createTodoInput: CreateTodoInput,
+  ) {
     return this.todoService.create(createTodoInput);
   }
 
-  updateTodo() {}
+  @Mutation(() => Todo, { name: 'updateTodo' })
+  updateTodo(
+    @Args('updateTodoInput')
+    updateTodoInput: UpdateTodoInput,
+  ) {
+    return this.todoService.update(updateTodoInput);
+  }
 
   removeTodo() {}
 }
--- src/todo/todo.service.ts
+++ src/todo/todo.service.ts
@@ -1,12 +1,22 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { Todo } from './entity/todo.entity';
-import { CreateTodoInput } from './dto/inputs/create-todo.input';
+import {
+  CreateTodoInput,
+  UpdateTodoInput,
+} from './dto/inputs';
 
 @Injectable()
 export class TodoService {
   private todos: Todo[] = [
     { id: 1, description: 'Piedra del Alma', done: false },
-    { id: 2, description: 'Piedra del Espacio', done: false },
+    {
+      id: 2,
+      description: 'Piedra del Espacio',
+      done: false,
+    },
     { id: 3, description: 'Piedra del Poder', done: false },
   ];
 
@@ -17,16 +27,35 @@ export class TodoService {
   findOne(id: number): Todo {
     const todo = this.todos.find((todo) => todo.id === id);
 
-    if (!todo) throw new NotFoundException(`Todo with ${id} not found`);
+    if (!todo)
+      throw new NotFoundException(
+        `Todo with ${id} not found`,
+      );
     return todo;
   }
 
   create(createTodoInput: CreateTodoInput): Todo {
     const todo = new Todo();
     todo.description = createTodoInput.description;
-    todo.id = Math.max(...this.todos.map((todo) => todo.id), 0) + 1;
+    todo.id =
+      Math.max(...this.todos.map((todo) => todo.id), 0) + 1;
     this.todos.push(todo);
-    
+
     return todo;
+  }
+
+  update(updateTodoInput: UpdateTodoInput) {
+    const { id, description, done } = updateTodoInput;
+    const todoToUpdate = this.findOne(id);
+
+    if (description)
+      todoToUpdate.description = description;
+    if (done !== undefined) todoToUpdate.done = done;
+
+    this.todos = this.todos.map((todo) => {
+      return todo.id === id ? todoToUpdate : todo;
+    });
+
+    return todoToUpdate;
   }
 }
